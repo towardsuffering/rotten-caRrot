20735607b0aba163e427248ff1dbaa4995da3e74
towardsuffering <christopher.rotunno@outlook.com>
2022-12-13 17:05:09 -0600

    personal landing page and blog

---
+++ content/sections/about/about.md
@@ -0,0 +1,14 @@
+---
+imageSrc: '../../images/clement-h-95YRwf6CNw8-unsplash.jpg'
+imageAlt: 'About Gatsby Theme Portfolio Minimal'
+---
+
+🌟 I enjoy picking up new ideas and putting them into practice because they push the boundaries of my preconceived notions.
+
+📦 I put my ideas to the test by asking for other people's feedback in order to think beyond the box.
+
+💼 I have work experience in retail, corporate, and startup environments working in remote and onsite settings.
+
+🌎 My professional goal is to raise service standards because I believe higher standards will make the world is a better place.
+
+Photo by <a href="https://unsplash.com/@clemhlrdt" target="_blank" rel="nofollow noopener noreferrer" aria-label="External Link"><u>Clément H</u></a> on Unsplash
---
+++ src/pages/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+    AboutSection,
+    ArticlesSection,
+    ContactSection,
+    HeroSection,
+    InterestsSection,
+    Page,
+    ProjectsSection,
+    Seo,
+} from 'gatsby-theme-portfolio-minimal';
+
+export default function IndexPage() {
+    return (
+        <>
+            <Seo title="rotten_caRrot" />
+            <Page useSplashScreenAnimation>
+                <HeroSection sectionId="hero" />
+                <ArticlesSection sectionId="blog" heading="Blog" sources={['Medium']} />
+                <AboutSection sectionId="about" heading="About" />
+                <InterestsSection sectionId="tools" heading="Tools" />
+                <ProjectsSection sectionId="interests" heading="Interests" />
+                <ContactSection sectionId="contact" heading="Contact" />
+            </Page>
+        </>
+    );
+}
